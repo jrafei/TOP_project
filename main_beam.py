9b4e7a8ab1968c29042fe8d27e578abe96da991e
jrafei <jana.eltayeb-el-rafei@etu.utc.fr>
2023-12-09 19:14:00 +0100

    add_new_results

--- main_beam.py
+++ main_beam.py
@@ -4,16 +4,16 @@ import time
 
 
 # Nom du fichier CSV
-nom_fichier_csv = 'output_all_beam.csv'
+nom_fichier_csv = 'output_all_beam_p1.csv'
 
 # En-têtes des colonnes
 en_tetes = ['instance', 'tmps_max', 'nombre_clients', 'nombre_tournees', 'profit_total', 'temps_execution','wmax']
 
 #list = ['a','b','c','d','e','f']
-#set = ['Set_32_234/p1.','Set_21_234/p2.','Set_33_234/p3.', "Set_100_234/p4.",'Set_66_234/p5.','Set_64_234/p6.',"Set_102_234.p7."]
-#chiffre = ['2','3','4']
-set = ['Set_21_234/p2.']
-chiffre = ['3']
+set = ['Set_32_234/p1.']#,'Set_21_234/p2.','Set_33_234/p3.', "Set_100_234/p4.",'Set_66_234/p5.','Set_64_234/p6.',"Set_102_234.p7."]
+chiffre = ['2','3','4']
+#set = ['Set_21_234/p2.']
+#chiffre = ['3']
 list = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r']
 
 # Ouvrir un fichier CSV pour l'écriture
@@ -45,7 +45,7 @@ with open(nom_fichier_csv, mode='w', newline='') as fichier_csv:
                 liste_clients, pt_depart, pt_arrivee = getNode_respect_time(top['points'],top['tmax'])
                 
                 start_time = time.time()
-                for w in range (1,200) : 
+                for w in range (1,10) : 
                     mus = beam(liste_clients,pt_depart,pt_arrivee, top['tmax'], top['m'], w)            
                     profit = 0
                     for mu in mus:
@@ -61,7 +61,7 @@ with open(nom_fichier_csv, mode='w', newline='') as fichier_csv:
                         wmax = w
     
                 end_time = time.time()
-                donnees = [y+"."+x, top['tmax'], top['n'], top['m'], profit, round((end_time - start_time),4),wmax]
+                donnees = ['p1.'+y+"."+x, top['tmax'], top['n'], top['m'], profit, round((end_time - start_time),4),wmax]
                 #print_to_file(file,"Profit total : ", profit)
                 writer.writerow(donnees)
                 
